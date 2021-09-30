a213916e12ed1a3ca1f3cfcc186cb98779048054
Lukas Hein <lukas.hein@amcongmbh.de>
2021-09-30 17:19:03 +0200

    Add C# project

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (var hiddenForm = new Form())
+            using (var mag = new Karna.Magnification.Magnifier(hiddenForm) { Magnification = 1f })
+            {
+                Application.Run();
+            }
+        }
+    }
+}
